396c2ebbcdd6d88cc883676f4827e9c29a024b65
Cha_K23 <charlie@cartell.ie>
2020-10-18 16:07:31 +0000

    adding charging point location  markers and cluster

--- JS/charger_data.js
+++ JS/charger_data.js
@@ -1,33 +1,47 @@
 function getData(cb) {
 
-var xhr = new XMLHttpRequest();
+    var xhr = new XMLHttpRequest();
 
 
-xhr.open("GET", "https://api.openchargemap.io/v3/poi/?output=json&countrycode=IE");
-xhr.send();
+        xhr.open("GET", "https://api.openchargemap.io/v3/poi/?output=json&countrycode=IE");
+        xhr.send();
 
 
-xhr.onreadystatechange = function() {
-    if(this.readyState == 4 && this.status == 200) {
-        cb(JSON.parse(this.response));        
-    }
-    
-
-
+        xhr.onreadystatechange = function() {
+            if(this.readyState == 4 && this.status == 200) {
+                cb(JSON.parse(this.response));        
+            }
+        };    
 };
     
-}
+var locations = [];
 
-    getData(function(data) {
+
+getData(function(data) {
     // loop through and add to array [{lat: 54.508650, lng: -1.353513}, {lat: 54.508650, lng: -1.353513}, {lat: 54.508650, lng: -1.353513}]
-    //let markers = data.map(function(location, i){
-    //            return new google.maps.Marker({
-    //                position: location, 
-    //            });
-    
     for(var index in data){
-        console.log(data[index]['AddressInfo']['Latitude'] + ' ' + data[index]['AddressInfo']['Longitude'] + ' ' + data[index]['AddressInfo']['Town']);        
-    }   
-    
+        var loc = {};  // create the object called 'loc'
+        loc["lat"] = data[index]['AddressInfo']['Latitude'];  // assign {'lat':} for loc-Object
+        loc["lng"] = data[index]['AddressInfo']['Longitude'];  // assign {'lng':} for loc-Object
+        locations.push(loc);  // push loc-Object into locations-array
+    }
+    initMap();
 });
 
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 53.361838, lng: -7.654365 },
+        zoom: 6.5,
+    });
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+        });
+    });
+
+    
+    var markerCluser = new MarkerClusterer(map, markers, {
+        imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+    });              
+};
